e8ad9220f60f40e77c683a084bc6de4d142561ed
Abdelrhman Ahmed <mrblackvanta@gmail.com>
2022-10-13 16:26:02 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import Details from "./components/Details";
+import Hero from "./components/Hero";
+import logo from "./images/logo.svg";
 import Signature from "./components/Signature";
 import "./App.scss";
 
 function App() {
-  const data = {};
+  const data = {
+    logo: logo,
+    heading: "We're coming soon",
+    description:
+      "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.",
+  };
 
   return (
     <div>
+      <main role="main">
+        <Details />
+        <Hero />
+      </main>
       <Signature />
     </div>
   );
